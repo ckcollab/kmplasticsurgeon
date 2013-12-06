e52c8100802ebe917f209dd5d4f84b800f68ef65
ckcollab <eric@ckcollab.com>
2013-12-05 20:48:01 -0800

    added about us page and some filler stuff, moved header/body nav into their own includes

---
+++ project/content/partials/_carousel.php
@@ -0,0 +1,42 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+			<div id="carousel" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#carousel" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel" data-slide-to="1"></li>
+				</ol>
+				
+				<!--
+				<a class="left carousel-control" href="#carousel" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left"></span>
+				</a>
+				<a class="right carousel-control" href="#carousel" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right"></span>
+				</a>
+				-->
+				
+				<div class="carousel-inner">
+					<div class="item active">
+						<img src="assets/img/carousel/hands_on_face.jpg">
+						<div class="hand-to-face caption">
+							<p class="fancy">You deserve it</p>
+							<span class="km-btn">Book <p>Now</p></span>
+						</div>
+					</div>
+					<div class="item">
+						<img src="assets/img/carousel/face_on_hands.jpg">
+						<div class="face-to-hand caption">
+							<p class="fancy">The new you</p>
+							<span class="km-btn">Book <p>Now</p></span>
+						</div>
+					</div>
+				</div>
+				
+				<img class="plumeria" src="assets/img/plumeria.png" alt="Plumeria Flower" width="190">
+				<img class="rocks" src="assets/img/rocks.png" alt="Rocks and Flowers" width="175">
+			</div>
+		</div>
+	</div>
+	
+</div>
