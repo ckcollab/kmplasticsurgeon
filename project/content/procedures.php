d8d09c35879ade6b76317ba6f035c0c3980c536b
ckcollab <eric@ckcollab.com>
2013-12-15 16:01:33 -0800

    removed procedures page

--- project/content/procedures.php
+++ project/content/procedures.php
@@ -1,18 +1,14 @@
-<?php include('partials/_body_nav.php'); ?>
-
 <div class="container" ng-init="init()">
 	<div class="row">
 		<div class="col-lg-12">
-			<p>The number of people choosing to have plastic surgery has soared in recent years. Technological advances have increased the options you can consider. But do your research before you consider any operation.</p>
-
-			<p>All surgery carries some risk. And while plastic surgery can improve a part of you body, it rarely improves your whole life. Make sure your goals are realistic.</p>
-
-			<p><b>To begin your quest for plastic surgery information, please click the part of the body you are interested in having a procedure on.</b></p>
-
             <iframe src='http://www.plasticsurgery.org/x8496.xml?key=22362f99a983d-3e0d-4758-84ed-a1094e26693a492131' width='100%' height='724' scrolling='auto' style='border:none;'></iframe>
 		</div>
 	</div>
 </div>
+
+<?php
+/*
+<?php include('partials/_body_nav.php'); ?>
 
 <div ng-cloak ng-show="show_procedures_for == 'face'">
     <div id="procedure_table_of_contents" class="table_of_contents_wrapper">
@@ -20,7 +16,7 @@
 
         <div class="container table_of_contents">
             <div class="row">
-                <div class="col-sm-4">
+                <div class="col-sm-offset-4 col-sm-8">
                     <h1>Face</h1>
 
                     <p>Explore a subcategory:</p>
@@ -28,8 +24,7 @@
                         <button class="btn btn-default" ng-click="subcategory = 'eyelid surgery'" ng-class="{active: subcategory == 'eyelid surgery'}">Eyelid Surgery</button>
                         <button class="btn btn-default" ng-click="subcategory = 'forehead lift'" ng-class="{active: subcategory == 'forehead lift'}">Forehead Lift</button>
                     </div>
-                </div>
-                <div class="col-sm-8">
+
                     <ol ng-show="subcategory == 'eyelid surgery'">
                         <li><a href="#eyelid_surgery_information">Eyelid surgery information</a>
                         <li><a href="#eyelid_what_to_expect">What to expect during your eyelid surgery consultation</a>
@@ -55,8 +50,9 @@
         <img class="divider" src="assets/img/divider-3x.png">
     </div>
 
-    <!-- Content -->
     <div ng-show="subcategory == 'eyelid surgery'">
         <?php include('partials/procedures/eyelid_surgery.php'); ?>
     </div>
 </div>
+*/
+?>
